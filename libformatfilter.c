0ab8fa0ef42cd2be5364a284aef9220ef465eeb1
Da_Macchi <damacchi91@gmail.com>
2016-09-10 15:31:52 +0200

    finiti i casi in cui non viene trovata la %n

--- libformatfilter.c
+++ libformatfilter.c
@@ -3,11 +3,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 
 int (*real_printf)(const char *format, ...);
 int (*real_fprintf)(FILE *stream, const char *format, ...);
 int (*real_sprintf)(char *str, const char *format, ...);
 int (*real_snprintf)(char *str, size_t size, const char *format, ...);
+int (*real_scanf)(const char *format, ...);
+int (*real_fscanf)(FILE *stream, const char *format, ...);
+int (*real_sscanf)(const char *str, const char *format, ...);
 
 const char *i, *error;
 void *handler;
@@ -22,7 +26,7 @@ int printf (const char *format, ...) {
       i++;
       if(*i == 'n'){
         exit(1);
-        /*trovato*/
+        syslog(LOG_WARNING, "%m");
       }
     }
   }
@@ -76,12 +80,159 @@ int fprintf(FILE *stream, const char *format, ...){
   return numofchar;
 }
 
-int sprintf(char *str, const char *format, ...){
-  return 0;
 
+
+
+int sprintf(char *str, const char *format, ...){
+  int numofchar;
+  for (i=format; *i; i++){
+    if (*i == '%'){
+      i++;
+      if(*i == 'n'){
+        exit(1);
+        /*trovato*/
+      }
+    }
+  }
+  handler = dlopen("libc.so.6", RTLD_LAZY);
+  if (!handler) {
+    fputs(dlerror(), stderr);
+    exit(1);
+  }
+  dlerror();
+  real_sprintf = dlsym(handler, "vsprintf");
+  if ((error = dlerror())) {
+    fputs(error, stderr);
+    exit(1);
+  }
+  va_start(argument_list, format);
+  numofchar = real_sprintf(str, format, argument_list);
+  va_end(argument_list);
+  dlclose(handler);
+  return numofchar;
 }
 
-int snprintf(char *str, size_t size, const char *format, ...){
-  return 0;
 
+
+
+int snprintf(char *str, size_t size, const char *format, ...){
+  int numofchar;
+  for (i=format; *i; i++){
+    if (*i == '%'){
+      i++;
+      if(*i == 'n'){
+        exit(1);
+        /*trovato*/
+      }
+    }
+  }
+  handler = dlopen("libc.so.6", RTLD_LAZY);
+  if (!handler) {
+    fputs(dlerror(), stderr);
+    exit(1);
+  }
+  dlerror();
+  real_snprintf = dlsym(handler, "vsnprintf");
+  if ((error = dlerror())) {
+    fputs(error, stderr);
+    exit(1);
+  }
+  va_start(argument_list, format);
+  numofchar = real_snprintf(str, size, format, argument_list);
+  va_end(argument_list);
+  dlclose(handler);
+  return numofchar;
+}
+
+
+
+int scanf(const char *format, ...){
+  int numofinputs;
+  for (i=format; *i; i++){
+    if (*i == '%'){
+      i++;
+      if(*i == 'n'){
+        exit(1);
+        /*trovato*/
+      }
+    }
+  }
+  handler = dlopen("libc.so.6", RTLD_LAZY);
+  if (!handler) {
+    fputs(dlerror(), stderr);
+    exit(1);
+  }
+  dlerror();
+  real_scanf = dlsym(handler, "vscanf");
+  if ((error = dlerror())) {
+    fputs(error, stderr);
+    exit(1);
+  }
+  va_start(argument_list, format);
+  numofinputs = real_scanf(format, argument_list);
+  va_end(argument_list);
+  dlclose(handler);
+  return numofinputs;
+}
+
+
+
+int fscanf(FILE *stream, const char *format, ...){
+  int numofinputs;
+  for (i=format; *i; i++){
+    if (*i == '%'){
+      i++;
+      if(*i == 'n'){
+        exit(1);
+        /*trovato*/
+      }
+    }
+  }
+  handler = dlopen("libc.so.6", RTLD_LAZY);
+  if (!handler) {
+    fputs(dlerror(), stderr);
+    exit(1);
+  }
+  dlerror();
+  real_fscanf = dlsym(handler, "vfscanf");
+  if ((error = dlerror())) {
+    fputs(error, stderr);
+    exit(1);
+  }
+  va_start(argument_list, format);
+  numofinputs = real_fscanf(stream, format, argument_list);
+  va_end(argument_list);
+  dlclose(handler);
+  return numofinputs;
+}
+
+
+
+int sscanf(const char *str, const char *format, ...){
+  int numofinputs;
+  for (i=format; *i; i++){
+    if (*i == '%'){
+      i++;
+      if(*i == 'n'){
+        exit(1);
+        /*trovato*/
+      }
+    }
+  }
+  handler = dlopen("libc.so.6", RTLD_LAZY);
+  if (!handler) {
+    fputs(dlerror(), stderr);
+    exit(1);
+  }
+  dlerror();
+  real_sscanf = dlsym(handler, "vsscanf");
+  if ((error = dlerror())) {
+    fputs(error, stderr);
+    exit(1);
+  }
+  va_start(argument_list, format);
+  numofinputs = real_sscanf(str, format, argument_list);
+  va_end(argument_list);
+  dlclose(handler);
+  return numofinputs;
 }
--- tester.c
+++ tester.c
@@ -6,18 +6,52 @@
 
 int main(void) {
   FILE *fp;
-  char *str_s;
-  char *str_sn;
-  size_t sn_len = 8;
+  char str_s[100];
+  char str_sn[100];
+  size_t sn_len = 16;
+  int return_value;
+  int scan_intarg;
+  char scan_chararg;
+  char scan_word[20];
+  int percn;
 
-  fp = fopen ("file.txt", "a");
-  printf("A robot may not injure a human bein%c\n", 'g');
-  fprintf(fp, "A robot may not injure a human bein%c\n", 'g');
-  /*sprintf(str_s, "A robot may not injure a human bein%c\n", 'g');
+  fp = fopen ("file.txt", "w+");
+
+  //printf test
+  return_value = printf("PRINT: A robot may not injure a human bein%c\n", 'g');
+  printf("printf returned: %d\n", return_value);
+
+  //fprintf test
+  return_value = fprintf(fp, "FPRINT: A robot may not injure a human bein%c\n", 'g');
+  printf("fprintf returned: %d\n", return_value);
+
+  //sprintf test
+  return_value = sprintf(str_s, "SPRINT: A robot may not injure a human bein%c\n", 'g');
   puts(str_s);
-  snprintf(str_sn, sn_len, "A robot may not injure a human bein%c\n", 'g');
-  puts(str_sn);*/
-  fclose(fp);
+  printf("sprintf returned: %d\n", return_value);
 
+  //snprintf test
+  return_value = snprintf(str_sn, sn_len, "SNPRINT: A robot may not injure a human bein%c\n", 'g');
+  puts(str_sn);
+  printf("snprintf returned: %d\n", return_value);
+
+  //scanf test
+  puts("Type an int and a char : ");
+  return_value = scanf("%d %c", &scan_intarg, &scan_chararg);
+  printf("You typed: %d, %c\nscanf returned: %d\n", scan_intarg, scan_chararg, return_value);
+
+  //fscanf test
+  rewind(fp);
+  return_value = fscanf(fp,"%s", scan_word);
+  printf("The first word in file.txt is: %s\nfscanf returned: %d\n", scan_word, return_value);
+
+  //sscanf test
+  return_value = sscanf(str_s, "%s", scan_word);
+  printf("The first word in str_s string is: %s\nsscanf returned: %d\n", scan_word, return_value);
+
+  //%n tests
+  //printf("%n", &percn);
+
+  fclose(fp);
   return 0;
 }
