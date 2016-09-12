fd4663e6ff0d97896443c19a67f929efbd212d40
Da_Macchi <damacchi91@gmail.com>
2016-09-12 09:59:03 +0200

    ultimi commenti aggiunti

--- libformatfilter.c
+++ libformatfilter.c
@@ -1,11 +1,15 @@
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // @@@  This library wraps all the functions from the printf and scanf families.   @@@
+// @@@  It must be loaded before th standard C library using LD_PRELOAD.           @@@
 // @@@  Its purpose is to catch dangerous format strings containing the %n format  @@@
 // @@@  specifier or the $ sign. When one of these is found, the user is asked     @@@
 // @@@  to choose to terminate the program or call the original function.          @@@
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Davide Macchi - 760461@@@@@
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@POLIMI - Piattaforme software per la rete MOD2@@@@@
+// @@@  Original functions:                                                        @@@
+// @@@  http://linux.die.net/man/3/printf                                          @@@
+// @@@  http://linux.die.net/man/3/scanf                                           @@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Davide Macchi - 760461 @@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ POLIMI - Piattaforme software per la rete MOD2 @@@@
 
 #include <dlfcn.h>
 #include <string.h>
@@ -14,6 +18,7 @@
 #include <stdlib.h>
 #include <syslog.h>
 
+//There is a pointer to a function for each function wrapped
 int (*real_printf)(const char *format, ...);
 int (*real_fprintf)(FILE *stream, const char *format, ...);
 int (*real_sprintf)(char *str, const char *format, ...);
@@ -26,6 +31,8 @@ const char *i;
 va_list argument_list;
 void *handler;
 
+//Read format string and find occurrences of '$' and %n in conversion specifications.
+//It works when flags, width, precision and length fields are used.
 int find_n(const char *format){
   for (i=format; *i; i++){
     if (*i == '%'){
@@ -47,6 +54,7 @@ int find_n(const char *format){
   return 0;
 }
 
+//Load the needed function, refered by name argument, from the standard C library.
 void *get_function (char *name) {
   void *real_function;
   const char *error;
@@ -64,6 +72,7 @@ void *get_function (char *name) {
   return real_function;
 }
 
+//When %n or $ is found, the user can terminate the program or go on
 void n_found () {
   char x;
   puts("WARNING: dangerous use of a format strig detected! Press [x] to continue or any other key to exit");
@@ -73,6 +82,16 @@ void n_found () {
     exit(1);
   }
 }
+
+
+//--------------------------------------------------------------------------------------------------------------
+
+
+//There are 7 wrappers: printf, fprintf, sprintf, snprintf, scanf, fscanf, sscanf.
+//They take a format strings and call find_n. On success a warnign message is sent to the syslog
+//and n_found is called. If the user say so or find_n doesn't succeed, the original function is called.
+//Actualy thw wrappers call the "v" version of the corresponding function that can take
+//as an argument a list of arguments (http://linux.die.net/man/3/stdarg)
 
 int printf (const char *format, ...) {
   int numofchar;
